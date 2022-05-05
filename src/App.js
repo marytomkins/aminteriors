f2d82195b7cb5dfcb864ef2755e1ab1f76317f3d
mct125 <mct125@scarletmail.rutgers.edu>
2022-05-05 12:19:47 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './templogo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <br/>
+        <br/>
+        <br/>
+        <p>
+          Nothing to see here. Yet.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
